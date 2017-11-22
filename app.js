7b69f81d63f0f59fc22af6b8391d297156839aa7
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-11-22 11:54:55 -0500

    initial lottery app skeleton

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// set up the server
+const http = require('http');
+
+// gaining access to the file system
+const fs = require('fs');
+
+http.createServer(function (req, res){
+  fs.readFile('index.html', function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+
+}).listen(8080);

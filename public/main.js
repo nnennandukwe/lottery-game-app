471544514da7317cb4c818bf5e45f3e9d700cbf4
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-12-12 03:02:10 -0500

    i don't know what i'm doing right now.

--- public/main.js
+++ public/main.js
@@ -21,9 +21,90 @@ console.log(random)
   if(first == random || second == random || third == random){
     console.log('You Win!')
     document.getElementById("resultMessage").innerHTML = "YOU WIN";
+
+    const wins = this.parentNode.parentNode.childNodes[1].innerText
+    const losses = this.parentNode.parentNode.childNodes[3].innerText
+    
     }else{
       console.log("You LOST!")
       document.getElementById("resultMessage").innerHTML = "YOUR LOSS, BUDDY.";
     }
 }
 })
+
+//Add listeners to elements
+
+let attended = document.getElementsByClassName('fa-thumbs-up')
+let late = document.getElementsByClassName('fa-thumbs-down')
+let deleteStudent = document.getElementsByClassName('fa-trash')
+
+Array.from(attended).forEach((element)=>{
+  element.addEventListener('click',function(){
+    console.log('click')
+    //get name and element from DOM
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const lastname = this.parentNode.parentNode.childNodes[3].innerText
+
+    //fetch request
+    fetch('studAttended',{
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'lastname': lastname,
+        'att': 'On time'
+      })
+    }).then(response =>{
+      if (response.ok) return response.json()
+    }).then(data=>{
+      console.log(data)
+      window.location.reload(true)
+    })
+  })
+});
+
+Array.from(late).forEach((element)=>{
+  element.addEventListener('click',function(){
+    //get name and element from DOM
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const lastname = this.parentNode.parentNode.childNodes[3].innerText
+
+    //fetch request
+    fetch('studAttended',{
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'lastname': lastname,
+        'att': 'Late'
+      })
+    }).then(response =>{
+      if (response.ok) return response.json()
+    }).then(data=>{
+      console.log(data)
+      window.location.reload(true)
+    })
+  })
+});
+
+Array.from(deleteStudent).forEach((element)=>{
+  element.addEventListener('click',function(){
+    //get name and element from DOM
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const lastname = this.parentNode.parentNode.childNodes[3].innerText
+
+    //fetch request
+    fetch('delete',{
+      method: 'delete',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'lastname': lastname
+      })
+    }).then(response =>{
+      window.location.reload()
+    })
+  })
+});
+
+//do fetch request
